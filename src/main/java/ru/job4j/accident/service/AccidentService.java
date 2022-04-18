667d4f2d1036cfdd09eb0f71f1cb3f0c8b233f2b
mirraim <mirraim@gmail.com>
2022-04-18 21:53:27 +0500

    2. IndexControl. Таблица и вид. [#2092]

---
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -0,0 +1,22 @@
+package ru.job4j.accident.service;
+
+import org.springframework.stereotype.Service;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.repository.AccidentMem;
+
+import java.util.List;
+
+@Service
+public class AccidentService {
+
+    private final AccidentMem accidentStorage;
+
+    public AccidentService(AccidentMem accidentStorage) {
+        this.accidentStorage = accidentStorage;
+    }
+
+    public List<Accident> getAccidents() {
+        accidentStorage.init();
+        return accidentStorage.getAccidents().values().stream().toList();
+    }
+}

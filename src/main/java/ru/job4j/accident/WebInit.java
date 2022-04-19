311ff0a40616ff917504fdccee738dff39479ada
mirraim <mirraim@gmail.com>
2022-04-19 17:44:56 +0500

    3. @ModelAttribute. Создание инцидента. [#261013]

--- src/main/java/ru/job4j/accident/WebInit.java
+++ src/main/java/ru/job4j/accident/WebInit.java
@@ -2,8 +2,10 @@ package ru.job4j.accident;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
@@ -11,6 +13,13 @@ public class WebInit implements WebApplicationInitializer {
     public void onStartup(ServletContext servletCxt) {
         AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
         ac.scan("ru.job4j.accident");
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        filter.setForceRequestEncoding(true);
+        FilterRegistration.Dynamic encoding = servletCxt.addFilter("encoding", filter);
+        encoding.addMappingForUrlPatterns(null, false, "/*");
 
         DispatcherServlet servlet = new DispatcherServlet(ac);
         ServletRegistration.Dynamic registration = servletCxt.addServlet("app", servlet);
---
+++ src/main/java/ru/job4j/accident/control/AccidentControl.java
@@ -0,0 +1,36 @@
+package ru.job4j.accident.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.repository.AccidentMem;
+
+@Controller
+public class AccidentControl {
+    private final AccidentMem accidents;
+
+    public AccidentControl(AccidentMem accidents) {
+        this.accidents = accidents;
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "/accident/create";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("accident", accidents.getById(id));
+        return "/accident/edit";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Accident accident) {
+        accidents.create(accident);
+        return "redirect:/";
+    }
+}
